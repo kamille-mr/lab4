543f8aed17de446b90cdaccf9be75315c8740491
kamille-mr <kamille.mille@mail.ru>
2022-12-14 20:10:47 +0300

    lab4

--- test/test_translator.cpp
+++ test/test_translator.cpp
@@ -1,3 +1,89 @@
 #include "translator.h"
-
 #include <gtest.h>
+
+
+TEST(Translator, can_create) {
+
+	ASSERT_NO_THROW(Translator t("123"));
+}
+
+TEST(Translator, can_get_postfix){
+
+	Translator t("(2+7)*9-60/2");
+	ASSERT_NO_THROW(t.GetPostfix());
+}
+
+TEST(Translator, can_get_infix){
+
+	Translator t("(2+7)*9-60/2");
+	ASSERT_NO_THROW(t.GetInfix());
+}
+
+TEST(Translator, can_calculate){
+
+	Translator t("(2+7)*9-60/2");
+	ASSERT_NO_THROW(t.Calculate());
+}
+
+TEST(Translator, current_calculating){
+
+	Translator t("(2+7)*9-60/2");
+	ASSERT_NO_THROW(t.Calculate(), 51);
+}
+
+TEST(Translator, current_calculating_negative){
+
+	Translator t("30*2-70+4*(12/6)");
+	ASSERT_NO_THROW(t.Calculate(), -2);
+}
+
+TEST(Translator, current_postfix_form){
+
+	Translator t("6+2*1");
+	ASSERT_EQ("621*+", t.GetPostfix());
+}
+
+TEST(Translator, current_infix_form){
+
+	Translator t("6+2*1");
+	ASSERT_EQ("6+2*1", t.GetInfix());
+
+}
+
+TEST(Translator, can_parse_digits){
+
+	Translator t("1");
+	ASSERT_EQ(t.Calculate(), 1);
+}
+
+TEST(Translator, can_add){
+
+	Translator t("17+32");
+	ASSERT_EQ(49.0, t.Calculate());
+}
+
+TEST(Translator, can_subtract){
+
+	Translator t("32-17");
+	ASSERT_EQ(15.0, t.Calculate());
+}
+
+TEST(Translator, can_subtract_negative) {
+
+	Translator t("17-32");
+	ASSERT_EQ(-15.0, t.Calculate());
+
+}
+
+TEST(Translator, can_multiply){
+
+	Translator t("7*7");
+	ASSERT_EQ(49.0, t.Calculate());
+}
+
+TEST(Translator, can_divide){
+
+	Translator t("11/2");
+	ASSERT_EQ(5.5, t.Calculate());
+}
+
